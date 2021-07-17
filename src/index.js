e07210831757dfe9b5626f8deb988b3725db541c
Blas <blasgonzalez2414@gmail.com>
2021-07-16 20:28:16 -0400

    modificar index.js usando buenas practicas(prettier)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
+import { promises as fs } from "fs";
+import moment from "moment";
 
-import {promises as fs} from "fs"
- import moment from "moment"
+(async () => {
+  const [template] = await Promise.all([
+    fs.readFile("./src/README.md.tpl", { encoding: "utf-8" }),
+  ]);
+  const fechaNacimiento = "1996-12-30";
+  const edad = moment().diff(fechaNacimiento, "years");
 
-
-( async () => {
-    const [template] = await Promise.all([ fs.readFile("./src/README.md.tpl",{encoding:"utf-8"})]);
-    const fechaNacimiento = "1996-12-30";
-    const edad = moment().diff(fechaNacimiento, 'years');
-    
-    const nuevoReadme = template.replace('%{{edad}}%', edad);
-    await fs.writeFile("./README.md", nuevoReadme);
+  const nuevoReadme = template.replace("%{{edad}}%", edad);
+  await fs.writeFile("./README.md", nuevoReadme);
 })();
-
-
